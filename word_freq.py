ebf60dc984092a6a06fcfd35ec5ede9f4ae34ba5
Kmoonj <aa2933159aa@naver.com>
2022-04-14 20:34:13 +0900

    left right

--- word_freq.py
+++ word_freq.py
@@ -23,5 +23,5 @@ for i in range(linenum):
 word_list = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
 
 for k,v in word_list[:num]:
-    print(str(k).ljust(6),str(v).rjust(6))
+    print(str(k).ljust(10),str(v).rjust(3))
 
