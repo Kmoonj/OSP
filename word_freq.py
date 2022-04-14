be5b13513512c2415916bd0d4a3fc8dfe4268a2d
Kmoonj <aa2933159aa@naver.com>
2022-04-14 19:19:18 +0900

    for1

--- word_freq.py
+++ word_freq.py
@@ -3,5 +3,14 @@
 import sys
 import num
 
+word.txt = sys.argv[1]
+num = int(sys.argv[2])
 fopen = open(word.txt)
 lines = fopen.readlines()
+linenum = len(lines)
+word_count = {}
+
+for i in range(linenum)
+    lines[i] = 
+    word_list = lines[i].split()
+
